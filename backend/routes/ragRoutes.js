983506e7fbaf0ebb3f0d882a944b053a50282885
Vejandla Chakrish <112060547+Vejandlachakrish@users.noreply.github.com>
2025-11-07 02:01:03 +0530

    Delete backend directory

--- backend/routes/ragRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { runRAG } = require('../controllers/ragController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/ask', protect, runRAG);
-
-module.exports = router;
--- backend/utils/embedder.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-import numpy as np
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def get_embeddings(text_chunks):
-    return model.encode(text_chunks)
