6ca1c51ff2cda47356ebb3331deb9becaed1f3f7
Vejandla Chakrish <112060547+Vejandlachakrish@users.noreply.github.com>
2025-11-07 02:01:14 +0530

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white text-center py-4 mt-10">
-      <p>© {new Date().getFullYear()} RAG-PDF-DoubtSolver. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
